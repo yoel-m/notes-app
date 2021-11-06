5588828c91ab2cd516d4656a3fc0dd4d3a3708d1
Yoel <ymalekan@gmail.com>
2021-11-06 13:20:11 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require ('fs')
+ 
+
+
+//fs.writeFileSync ('note.txt' , 'My name is Yoel')
+ 
+ //import { appendFileSync } from 'fs';
+
+ 
+ fs.appendFileSync('note.txt', 'data to append');
+  
