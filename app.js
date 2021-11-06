ed247cde980c55da5b6bcce4304b9d274afb79f6
Yoel <ymalekan@gmail.com>
2021-11-06 13:42:49 +0200

    Added a commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const fs = require ('fs')
 //fs.writeFileSync ('note.txt' , 'My name is Yoel')
  
  //import { appendFileSync } from 'fs';
+  
 
- 
+ // some changes
  fs.appendFileSync('note.txt', 'data to append');
   
